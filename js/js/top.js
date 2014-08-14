a3074cf6c247a277015fe8c4b6bb915439f39085
6cker845 <nick_fun6king@yahoo.co.jp>
2014-08-14 15:23:58 +0900

    モバイルのニュースデータをjsonで管理するためにモバイルデータ用のjsファイルを作成

--- js/js/top.js
+++ js/js/top.js
@@ -4,14 +4,14 @@ httpObj.onload = function(){
 var myData = JSON.parse(this.responseText);
 var liveTxt = "";
 var newsTxt = "";
+var mobileNewsTxt = "";
 for (var i=0; i<myData.live.length; i++){
 	liveTxt = liveTxt + "<p class='info'>" + myData.live[i].label + "</p><hr />";
 }
 for (var i=0; i<myData.news.length; i++){
 	newsTxt = newsTxt + "<p class='info'>" + myData.news[i].label + "<a href=" + myData.news[i].linkLabel + "><br>" + myData.news[i].detail + "</a></p><hr />";
 }
-console.log(liveTxt);
 document.getElementById("liveInfo").innerHTML = liveTxt;
-document.getElementById("newsInfo").innerHTML = newsTxt
+document.getElementById("newsInfo").innerHTML = newsTxt;
 }
 httpObj.send(null);
